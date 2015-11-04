1ad971af8b861ee6643273e9be0fea13353613b7
Anastasia Alt <anastasia.alt@gmail.com>
2015-11-04 15:34:24 -0500

    everything i am writing suckssss

--- main.js
+++ main.js
@@ -3,11 +3,20 @@ console.log('Hello World')
 var $promptInput = $('#prompt');
 var $log = $('#log');
 var $prompt = $('.prompt')
+var ducats = 10000;
 
-var addPrompt = function() {
-	console.log(event.target);
-	var promptList = document.createElement('li');
-	promptList = $promptInput.val();
-	// $('#prompt').val();
-	$prompt.append(promptList);
+var addPrompt = function(event) {
+	var $promptInput = $('#prompt').val();
+	if (event.true) {};
+	if (event.keyCode===13) {
+		console.log('pressed enter')
+		console.log($promptInput);
+	if ($promptInput==='purse') {
+		$('li').text('Shylock Bot pulls out his purse containing')
+	};}
+	$promptInput.val('');
 };	
+
+
+$(document).on('keypress',addPrompt);
+$('#submit').on('click',addPrompt);
